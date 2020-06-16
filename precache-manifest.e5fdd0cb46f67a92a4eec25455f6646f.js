c8fb7ff6af6734bb5bc82e2c17b13042bba9d5a1
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-06-16 12:40:30 +0530

    Updates

---
+++ precache-manifest.e5fdd0cb46f67a92a4eec25455f6646f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf4ddfbe4534a30c4952f390f6427a5",
+    "url": "/Favorites-React-App/index.html"
+  },
+  {
+    "revision": "a6f359b31067be27f74c",
+    "url": "/Favorites-React-App/static/css/main.c69bebdf.chunk.css"
+  },
+  {
+    "revision": "2c6364654e5cbe43c03f",
+    "url": "/Favorites-React-App/static/js/2.cc14042e.chunk.js"
+  },
+  {
+    "revision": "a6f359b31067be27f74c",
+    "url": "/Favorites-React-App/static/js/main.e2c73b32.chunk.js"
+  },
+  {
+    "revision": "0fdff10e75a697aab02a",
+    "url": "/Favorites-React-App/static/js/runtime~main.e79b3fbb.js"
+  }
+]);
